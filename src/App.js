d1c9964886962b074422c805ff29af761f88f2f4
tuannx21 <tuannguyen@DESKTOP-1E1AV4K.localdomain>
2020-02-29 11:01:24 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import './scss/main.scss'
-import Step from './components/Step'
 import Stepper from './components/Stepper'
 
 const stepLabels = ['Supplier', 'Basic Information', 'Lorem ispsum', 'Some dumb text', 'Vertical Stepper']
@@ -10,16 +9,11 @@ const App = props => {
 
   return (
     <div className="container" style={{ marginTop: '80px' }}>
-      <Stepper>
-        {stepLabels.map((label, index) => (
-          <Step
-            order={index + 1}
-            label={label}
-            isActivated={index === currentStep - 1}
-            onClickStep={() => setCurrentStep(index + 1)}
-          />
-        ))}
-      </Stepper>
+      <Stepper
+        activeStep={currentStep}
+        stepLabels={stepLabels}
+        onChangeOrder={setCurrentStep}
+      />
     </div>
   )
 }
--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Step from './Step'
 
 const Stepper = props => {
-  const { children } = props
+  const { stepLabels, onChangeOrder, activeStep } = props
 
   return (
     <div className="stepper">
-      {children}
+      {stepLabels.map((label, index) => (
+        <Step
+          order={index + 1}
+          label={label}
+          isActivated={index === activeStep - 1}
+          onClickStep={() => onChangeOrder(index + 1)}
+        />
+      ))}
     </div>
   )
 }
